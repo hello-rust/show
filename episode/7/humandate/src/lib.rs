9c4b3bb9ebf1b4164bef550e88a28375cde8e53b
Matthias Endler <matthias-endler@gmx.net>
2018-05-31 23:36:22 +0200

    proptest found a bug

--- episode/7/humandate/src/lib.rs
+++ episode/7/humandate/src/lib.rs
@@ -20,6 +20,24 @@ impl From<ParseIntError> for Error {
     }
 }
 
+fn match_month(month: impl AsRef<str>) -> u32 {
+    match month.as_ref() {
+        "January" => 1,
+        "February" => 2,
+        "March" => 3,
+        "April" => 4,
+        "May" => 5,
+        "June" => 6,
+        "July" => 7,
+        "August" => 8,
+        "September" => 9,
+        "October" => 10,
+        "November" => 11,
+        "Dezember" => 12,
+        _ => unreachable!(),
+    }
+}
+
 pub fn parse(humandate: impl AsRef<str>) -> Result<chrono::NaiveDate, Error> {
     let parts: Vec<String> = humandate
         .as_ref()
@@ -35,21 +53,7 @@ pub fn parse(humandate: impl AsRef<str>) -> Result<chrono::NaiveDate, Error> {
     let (day_str, _suffix) = day_human.split_at(2);
 
     let day = day_str.parse::<u32>()?;
-    let month = match month_human.as_ref() {
-        "January" => 1,
-        "February" => 2,
-        "March" => 3,
-        "April" => 4,
-        "May" => 5,
-        "June" => 6,
-        "July" => 7,
-        "August" => 8,
-        "September" => 9,
-        "October" => 10,
-        "November" => 11,
-        "Dezember" => 12,
-        _ => unreachable!(),
-    };
+    let month = match_month(month_human);
 
     // Avoid chrono panic: 'No such local time'
     // see chrono/src/offset/mod.rs:145:34
@@ -74,14 +78,43 @@ mod test {
     }
 
     proptest! {
-        #[test]
-        fn doesnt_crash(ref s in "\\PC*") {
-            parse(s);
-        }
+    #[test]
+    fn doesnt_crash(ref s in "\\PC*") {
+        parse(s);
+    }
 
-        #[test]
-        fn parses_all_valid_dates(ref s in "([0-2](1st|2nd|3rd|[4-9]th)|10th|20th|30th|31st) of (January) -?(0|[1-9][0-9]{4})") {
-            parse(s).unwrap();
-        }
+    #[test]
+    fn parses_all_valid_dates(ref s in "([0-2](1st|2nd|3rd|[4-9]th)|10th|20th|30th|31st) of (January) -?(0|[1-9][0-9]{4})") {
+        parse(s).unwrap();
+    }
+
+    #[test]
+    fn parses_date_back_to_original(y in 0i32..10000,
+                                    m in 1u32..13, d in 1u32..32) {
+        let month = match m {
+            1 => "January",
+            2 => "February",
+            3 => "March",
+            4 => "April",
+            5 => "May",
+            6 => "June",
+            7 => "July",
+            8 => "August",
+            9 => "September",
+            10 => "October",
+            11 => "November",
+            12 => "Decemeber",
+            _ => unreachable!(),
+        };
+
+        let suffix = match d {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
+        let date = parse(&format!("{:02}{} of {} {:02}", d, suffix, month, y)).unwrap();
+        prop_assert_eq!((y, m, d), (date.year(), date.month(), date.day()));
+    }
     }
 }

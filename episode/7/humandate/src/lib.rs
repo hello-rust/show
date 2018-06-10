03b6d19e0455b313e25d2eedd2c17a2f712f6cf3
Matthias Endler <matthias-endler@gmx.net>
2018-06-10 13:23:34 +0200

    Live code

--- episode/7/humandate/src/lib.rs
+++ episode/7/humandate/src/lib.rs
@@ -1,133 +1,132 @@
+extern crate chrono;
+
 #[cfg(test)]
 #[macro_use]
 extern crate proptest;
 
-extern crate chrono;
+use chrono::NaiveDate;
 
-use chrono::prelude::*;
-use std::convert::From;
-use std::num::ParseIntError;
+static MONTH_NAMES: [&str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    Parse,
-    InvalidDate,
+    ParseError,
+    InvalidDateError,
+    InvalidDayError,
 }
 
-impl From<ParseIntError> for Error {
-    fn from(_e: ParseIntError) -> Error {
-        Error::Parse
+impl From<std::num::ParseIntError> for Error {
+    fn from(T: std::num::ParseIntError) -> Error {
+        Error::ParseError
     }
 }
 
-fn numeric_month(month: impl AsRef<str>) -> Option<u32> {
-    let num = match month.as_ref() {
-        "January" => 1,
-        "February" => 2,
-        "March" => 3,
-        "April" => 4,
-        "May" => 5,
-        "June" => 6,
-        "July" => 7,
-        "August" => 8,
-        "September" => 9,
-        "October" => 10,
-        "November" => 11,
-        "December" => 12,
-        _ => return None,
-    };
-    Some(num)
+fn parse_month(month: &str) -> Option<usize> {
+    match MONTH_NAMES.iter().position(|&elem| elem == month) {
+        Some(index) => Some(index + 1),
+        None => None,
+    }
 }
 
-pub fn parse(humandate: impl AsRef<str>) -> Result<chrono::NaiveDate, Error> {
+fn parse_day(day_with_ordinal: &str) -> Result<u32, Error> {
+    let day: u32 = day_with_ordinal
+        .chars()
+        .take_while(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse::<u32>()?;
+    let ordinal = day_with_ordinal
+        .chars()
+        .skip_while(|c| c.is_digit(10))
+        .collect::<String>();
+
+    match (day, ordinal.as_ref()) {
+        (1, "st")
+        | (2, "nd")
+        | (3, "rd")
+        | (4...20, "th")
+        | (21, "st")
+        | (22, "nd")
+        | (23, "rd")
+        | (24...30, "th")
+        | (31, "st") => Ok(day),
+        _ => Err(Error::InvalidDayError),
+    }
+}
+
+/// Parses a human-readable date format:
+/// 4th of July 2018
+/// 2nd of March 2006
+/// 3rd of November 1975
+pub fn parse_date(humandate: impl AsRef<str>) -> Result<chrono::NaiveDate, Error> {
     let parts: Vec<&str> = humandate.as_ref().split_whitespace().collect();
     if parts.len() != 4 {
-        return Err(Error::Parse);
+        return Err(Error::ParseError);
     }
-    let day_human = &parts[0];
-    if day_human.len() != 4 {
-        return Err(Error::Parse);
-    }
-    let month_human = &parts[2];
-    let year = parts[3].parse::<i32>()?;
-    let (day_str, _suffix) = day_human.split_at(2);
+    let day: u32 = parse_day(parts[0])?;
 
-    let day = day_str.parse::<u32>()?;
-    let month = match numeric_month(month_human) {
-        Some(val) => val,
-        None => return Err(Error::Parse),
-    };
+    // TODO: parts[1] == of
 
-    // Avoid chrono panic: 'No such local time'
-    // see chrono/src/offset/mod.rs:145:34
-    NaiveDate::from_ymd_opt(year, month, day).ok_or(Error::InvalidDate)
+    let month: u32 = parse_month(parts[2]).ok_or_else(|| Error::ParseError)? as u32;
+    let year: i32 = parts[3].parse()?;
+    NaiveDate::from_ymd_opt(year, month, day).ok_or_else(|| Error::InvalidDateError)
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn test_valid() {
+    fn it_works() {
         assert_eq!(
-            parse("15th of May 2015"),
-            Ok(NaiveDate::from_ymd(2015, 5, 15))
+            parse_date("04th of September 2015"),
+            Ok(NaiveDate::from_ymd(2015, 9, 4))
         );
         assert_eq!(
-            parse("2nd of May 2015"),
-            Ok(NaiveDate::from_ymd(2015, 5, 2))
+            parse_date("05th of September 2015"),
+            Ok(NaiveDate::from_ymd(2015, 9, 5))
         );
-    }
-
-    #[test]
-    fn test_invalid() {
-        assert_eq!(parse("50th of May 2015"), Err(Error::InvalidDate));
+        assert_eq!(
+            parse_date("1st of September 2015"),
+            Ok(NaiveDate::from_ymd(2015, 9, 1))
+        );
     }
 
     proptest! {
-    #[test]
-    fn doesnt_crash(ref s in "\\PC*") {
-        parse(s);
-    }
+        #[test]
+        fn doesnt_crash(ref s in "\\PC*") {
+            parse_date(s);
+        }
 
-    #[test]
-    fn handles_invalid_words(ref s in "([0-9a-z]{1,5} ){3}[0-9a-z]{1,5}") {
-        let _ = parse(s);
-    }
+        #[test]
+        fn handles_invalid_words(ref s in "([0-9a-z]{1,5} ){3}[0-9a-z]{1,5}") {
+            let _ = parse_date(s);
+        }
 
-    #[test]
-    fn parses_all_valid_dates(ref s in "([0-2](1st|2nd|3rd|[4-9]th)|10th|20th|30th|31st) of (January) -?(0|[1-9][0-9]{4})") {
-        parse(s).unwrap();
-    }
-
-    #[test]
-    fn parses_date_back_to_original(y in 0i32..10000,
-                                    m in 1u32..13, d in 1u32..32) {
-            println!("y = {}, m = {}, d = {}", y, m, d);
-            let month = match m {
-                1 => "January",
-                2 => "February",
-                3 => "March",
-                4 => "April",
-                5 => "May",
-                6 => "June",
-                7 => "July",
-                8 => "August",
-                9 => "September",
-                10 => "October",
-                11 => "November",
-                12 => "December",
-                _ => unreachable!(),
-            };
-
-            let suffix = match d {
-                1 => "st",
-                2 => "nd",
-                3 => "rd",
+        #[test]
+        fn parses_date_back_to_original(y in 0i32..10000, m in 0usize..12, d in 1u32..32) {
+            let human_month = MONTH_NAMES[m];
+            let ordinal = match d {
+                1 | 21 | 31 => "st",
+                2 | 22 => "nd",
+                3 | 23 => "rd",
                 _ => "th",
             };
-            let date = parse(&format!("{:02}{} of {} {:02}", d, suffix, month, y));
-            prop_assert_eq!(NaiveDate::from_ymd_opt(y, m, d).ok_or(Error::InvalidDate), date);
+            let date_string = format!("{}{} of {} {}", d, ordinal, human_month,y);
+            println!("{}", date_string);
+            assert_eq!(parse_date(date_string), chrono::NaiveDate::from_ymd_opt(y, (m + 1) as u32, d).ok_or_else(|| Error::InvalidDateError));
         }
     }
 }
